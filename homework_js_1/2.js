ca8d187a350d5fb974bf97c7983f926af8328c35
evgeniynikolaevich <zplacesound@gmail.com>
2019-10-31 10:26:34 +0300

    add home work flex

---
+++ homework_js_1/2.js
@@ -0,0 +1,13 @@
+let m = 4,3;
+let n = 8,3;
+let target = 11,45;
+if (n<target || m <target){
+    console.log(n);
+    return n;
+}
+
+function find_p(height,length,width){
+    p =  2(length * height + height * width + length * height)
+    return p;
+
+}
