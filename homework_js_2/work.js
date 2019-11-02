b5afbaf31a59018326376723d8d0825c792ee6f5
evgeniynikolaevich <zplacesound@gmail.com>
2019-11-02 14:08:50 +0300

    add 13 lession

---
+++ homework_js_2/work.js
@@ -0,0 +1,24 @@
+
+//1.определить четное оно или нет
+let number = 4;
+if (number%2 ===0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+//2.ближайщее число к 10
+
+let m = 4,3;
+let n = 11,3;
+let target = 10;
+if (n<target || m <target){
+    console.log(n);
+    return n;
+}
+//3.для нахождени паралелипипеда
+function find_p(height,length,width){
+    p =  2(length * height + height * width + length * height)
+    return p;
+
+}
