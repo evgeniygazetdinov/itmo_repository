2530995346206337eea8278ce3b9671deac0b715
evgeniynikolaevich <zplacesound@gmail.com>
2019-12-18 11:11:10 +0300

    begin 4target on 7th homework js

--- homework_js_7/3-4.js
+++ homework_js_7/3-4.js
@@ -1,36 +1,61 @@
 //задание 3
 function initElements(){
-
-let form = document.createElement('form');
-let input = document.createElement('input');
-let checkbox = document.createElement('input');
-input.style.background ='yellow';
-checkbox.type = 'checkbox';
-checkbox.name = 'changeColor';
-checkbox.addEventListener('click',changer)
-function changer(event){
-    let change = event.target;
-    if(checkbox.checked === true){
-        input.style.background = 'red';
-        input.disabled = 'true';
-        console.log('enabled');
-    }
-    else{
-        input.style.background = 'yellow';
-
-        console.log(checkbox.checked)
-        console.log('disabled')
-        console.log(input);
-        input.removeAttribute('disabled');
+    let form = document.createElement('form');
+    let input = document.createElement('input');
+    let checkbox = document.createElement('input');
+    input.style.background ='yellow';
+    checkbox.type = 'checkbox';
+    checkbox.name = 'changeColor';
+    checkbox.addEventListener('click',changer)
+    function changer(event){
+        let change = event.target;
+        if(checkbox.checked === true){
+            input.style.background = 'red';
+            input.disabled = 'true';
         }
+        else{
+            input.style.background = 'yellow';
+            input.removeAttribute('disabled');
+            }
 }
 form.append(input);
 form.append(checkbox)
 return form
 }
+function createCheckbox(){
+    let elements = initElements();
+    document.body.append(elements);
+}
 
-let elements = initElements();
-document.body.append(elements);
+createCheckbox();
 
 
 
+
+function checkLoginPassword(){
+//    let elements = createElements();
+    let login = document.createElement('input');
+    let pass = document.createElement('input');
+    let form = document.createElement('form');
+    let tab = document.createElement('br');
+    let send = document.createElement('input')
+    pass.type = 'password';
+    send.type = 'button';
+    send.value = 'отправить';
+    form.append(tab);
+    form.append(login);
+    form.append(tab);
+    form.append(pass);
+    form.append(send);
+
+    document.getElementsByName("fo").focus()
+
+
+
+    console.log(form);
+    document.body.append(form);   
+
+
+}
+
+checkLoginPassword()
