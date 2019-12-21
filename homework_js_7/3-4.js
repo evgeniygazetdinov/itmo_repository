b966801c7b251d14bb112796b513234c7a59047b
evgeniynikolaevich <zplacesound@gmail.com>
2019-12-21 16:14:29 +0300

    allmost over 4th target

--- homework_js_7/3-4.js
+++ homework_js_7/3-4.js
@@ -34,22 +34,47 @@ createCheckbox();
 
 function checkLoginPassword(){
 //    let elements = createElements();
+    let error = document.createElement('p')    
     let login = document.createElement('input');
     let pass = document.createElement('input');
     let form = document.createElement('form');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
     let tab = document.createElement('br');
     let send = document.createElement('input')
+    error.innerText = '';
     pass.type = 'password';
     send.type = 'button';
     send.value = 'отправить';
+    p1.innerText = 'логин';
+    p2.innerText = 'пароль';
     form.append(tab);
+    form.append(p1);
     form.append(login);
     form.append(tab);
+    form.append(p2);
     form.append(pass);
+    form.append(tab);
     form.append(send);
+    
+    
+    function drawErrorNear(error,nameOfElement){
+        let pass = 'пароль';
+        let log = 'логин';
+        let warn = 'неверный';
+        error.style.background = 'red';
+        if(nameOfElement == 'login'){
+            error.innerText = warn+log;
+        } 
+        else{
+            error.innerText = warn+pass;
+        }
+    }
 
-    document.getElementsByName("fo").focus()
-
+    
+    
+    login.addEventListener("focusin", login.style.background = 'red';);
+    login.addEventListener("focusout", () => login.style.background = 'white');
 
 
     console.log(form);
