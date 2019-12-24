e46cb117b5e954a419090f224da75afcb5e8ff79
evgeniynikolaevich <zplacesound@gmail.com>
2019-12-24 23:45:27 +0300

    work on 8th hw js

--- homework_js_8/1.js
+++ homework_js_8/1.js
@@ -1,42 +1,76 @@
- so
-let score = document.getElementsByName("score"); 
-let score_canvas = document.getElementsByTagName("canvas")[1]; 
-score_canvas.width = (window.screen.widt);
-score_canvas.height = (window.screen.height);
-let ctx = canvas.getContext("2d");
-ctx.fillStyle = "red";
-ctx.fillRect(0,0,canvas.screen.width,canvas.screen.height);
-let game_field = document.getElementsByTagName("canvas")[0];
-game_field.width = window.screen.width;
-game_field.height = window.screen.height;
-let game_context = game_field.
-getContext("2d");
-game_context.fillStyle = "green";
-game_context.fillRect(0,0,900, 600);
-let score_point =document.createElement('p');
-score.innerText="Счет: "
+let canvas = document.getElementsByTagName('canvas')[0];
+let score_canvas = document.getElementsByTagName('score_canvas')[0]; 
+console.log(canvas);
+canvas.width = 900;
+canvas.height = 600;
+let ctx = canvas.getContext('2d');
+ctx.fillStyle = 'green';
+ctx.fillRect(0,0,900,600);
+//что рисовать 
 let tiger = {
-  img: 't.png',
-  x: 22,
-  y: 15,
-  width: 128,
-  height: 128
-};
-let rabbit = {                    
-  img: 'r.png',
-  x: corX,
-  y: corY,
-  width: 128,
-  height: 128
-};
-let dead_rabbit2 = {                    
-  img: 'dr.png',
-  x: 0,
-  y: 0,
-  width: 64,
-  height: 64
-};
+    img :'t.png',
+    x: 22,
+    y: 15,
+    width: 128,
+    heigth: 128
+}
+let rabbit = {
+    img: 'r.png',
+    x: getRandom(canvas.width - 128),
+    y: getRandom(canvas.height - 128),
+    width:128,
+    height:128
+}
+function clearImage(obj){
+    ctx.fillRect(obj.x,obj.y,obj.width,obj.height)
+}
+function draw_image(obj){
+    let img = new Image();
+    img.src = 'img/'+obj.img;
+    //привязка функции к событию
+    img.onload = () =>{
+        //метод контекста
+        ctx.drawImage(img, obj.x, obj.y,obj.width,obj.heigth);
+    }
 
+}
+function getRandom(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
+draw_image(tiger);
+draw_image(rabbit);
+//w
+//a
+//d
+//s
+function move(obj,event){
+    if(event.code ==="KeyW"){
+        clearImage(obj);
+        console.log('переместить вверх');
+        obj.y-=20;
+        draw_image(obj);
 
+    }
+    if(event.code ==="KeyS"){
+        clearImage(obj)
+        console.log('переместить вниз');
+        obj.y +=20;
+        draw_image(obj);
+    }
+    if(event.code ==="KeyA"){
+        clearImage(obj)
+        console.log('переместить влeво');
+        obj.x -=25;
+        draw_image(obj)
+ 
+    }
+    if(event.code ==="KeyD"){
+        clearImage(obj);
 
+        console.log('переместить вправо');
+        obj.x +=25;
+        draw_image(obj)
+    }
+}
+document.addEventListener('keydown',move.bind(document,tiger))
