945acd51cf46411bce0defad850e15e8d6ba4161
evgeniynikolaevich <zplacesound@gmail.com>
2019-12-25 10:04:34 +0300

    work on 8th hw-js

--- homework_js_8/1.js
+++ homework_js_8/1.js
@@ -1,29 +1,34 @@
-let canvas = document.getElementsByTagName('canvas')[0];
-let score_canvas = document.getElementsByTagName('score_canvas')[0]; 
-console.log(canvas);
-canvas.width = 900;
-canvas.height = 600;
-let ctx = canvas.getContext('2d');
-ctx.fillStyle = 'green';
-ctx.fillRect(0,0,900,600);
+function createPlace(order,name,width,height,color){
+  let place = document.getElementsByTagName(name)[order];
+  place.width = width;
+  place.height = height;
+  let ctx = place.getContext('2d');
+  ctx.fillStyle = color;
+  ctx.fillRect(0,0,900,600);
+  return place,ctx;
+}
+
+let canvas,ctx = createPlace(1,'canvas',900,600,'green');
+let score_canvas,score_ctx = createPlace(0,'canvas',900,100,'red');
 //что рисовать 
 let tiger = {
     img :'t.png',
-    x: 22,
-    y: 15,
+    x: 0,
+    y: 0,
     width: 128,
     heigth: 128
 }
 let rabbit = {
     img: 'r.png',
-    x: getRandom(canvas.width - 128),
-    y: getRandom(canvas.height - 128),
+    x: 50,//getRandom(canvas.width - 128),
+    y: 50,//getRandom(canvas.height - 128),
     width:128,
     height:128
 }
 function clearImage(obj){
     ctx.fillRect(obj.x,obj.y,obj.width,obj.height)
 }
+//TODO FIX FRAME
 function draw_image(obj){
     let img = new Image();
     img.src = 'img/'+obj.img;
@@ -39,7 +44,7 @@ function getRandom(max) {
 }
 
 draw_image(tiger);
-draw_image(rabbit);
+
 //w
 //a
 //d
@@ -74,3 +79,10 @@ function move(obj,event){
     }
 }
 document.addEventListener('keydown',move.bind(document,tiger))
+draw_image(rabbit)
+/*TODO
+1)tiger border
+2)tiger frame
+3)rabbit appear and disappear
+4) draw each rabbit in score 
+*/
