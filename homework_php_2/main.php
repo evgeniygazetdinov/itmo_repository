5db14b683d30beea579accc835bb2f26f0f59321
evgeniynikolaevich <zplacesound@gmail.com>
2020-01-08 06:27:18 +0300

    php2hw 3target

--- homework_php_2/main.php
+++ homework_php_2/main.php
@@ -1,5 +1,6 @@
 <?php
 echo 'Задание 1';
+echo '<br>';
 
 for($i=1;$i < 10; $i++){
     for($x=1;$x<10; $x++)
@@ -8,4 +9,20 @@ for($i=1;$i < 10; $i++){
      
 
 };
+?>
+<?php
+
+echo 'Задание 2';
+?>
+<form name="form" action="" method="get">
+  <input type="text" name="subject" id="subject" value="Car Loan">
+</form>
+<?php
+$x = $_GET['subject']; 
+echo $x;
+function calculate_y(){
+
+}
+
+
 ?>
