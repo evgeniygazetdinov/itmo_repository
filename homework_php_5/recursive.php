4f8cb5795793eb1f684597ca318c43f039fe672b
evgeniynikolaevich <zplacesound@gmail.com>
2020-01-14 10:19:00 +0300

    in progress 2tagret 5hw

--- homework_php_5/recursive.php
+++ homework_php_5/recursive.php
@@ -1,26 +1,37 @@
 <?php
 
 
-
-
 function makeRandomString($len) {
     $str = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $len);
     return $str;
 }
 
 function generateRandomName(){
-    $quan_words = rand(1,12);
+    $quan_words = rand(5,12);
     $name = makeRandomString($quan_words);
     return $name;
 }
-
-function create_folder($path){
+function fill_folder()
+{   $text_for_file = makeRandomString(rand(1000,10000));
+    $filename = generateRandomName();
+    $myfile = fopen("logs.txt", "a") or die("Unable to open file!");
+    fwrite($myfile, "\n". $text_for_file);
+    fclose($myfile);
+}
+function create_folder($path)
+{
     $name_folder = generateRandomName();
-    mkdir($path+)
+    $create = $path . "/".$name_folder;
+    mkdir($create);
+    echo $create;
 }
 
 
-function create_folders(){
-
+function create_folders()
+{
+    $path = getcwd();
+    create_folder($path);
 }
 
+create_folders();
+?>
